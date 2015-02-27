d41aed0619dd2acfa56433432e7aae4a2cd59c3b
Simon Elvery <elvery.simon@abc.net.au>
2015-02-27 13:46:54 +1000

    Support query strings

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
  */
 module.exports = function(url) {
 	var matches;
-	matches = url.match(/\/([0-9]+)(\/|$|-[0-9]+x[0-9]+-)/);
+	matches = url.match(/\/([0-9]+)(\/|(\?.*)?$|-[0-9]+x[0-9]+-)/);
 	return (matches) ? matches[1] || false : false;
 }
