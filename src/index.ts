089cd7326bfcd9b2f969bbd9ed0688bfd1769c4b
Simon Elvery <elvery.simon@abc.net.au>
2020-08-28 10:59:56 +1000

    Remove useless escapes

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
  * @return {string|false} The string representation of a CoreMedia ID or false if no CMID was found.
  */
 export const url2cmid = (url: string): false | string => {
-  const matches = url.match(/\/([0-9]+)(\/|([\?\#].*)?$|-[0-9]+x[0-9]+-)/);
+  const matches = url.match(/\/([0-9]+)(\/|([?#].*)?$|-[0-9]+x[0-9]+-)/);
   return matches ? matches[1] || false : false;
 };
 
